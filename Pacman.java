75a84283fd51e68ef240c6cdbc579b177d7db193
gkj11 <gkj11@scasd.org>
2016-03-03 16:29:27 -0500

    Update Pacman.java

--- Pacman.java
+++ Pacman.java
@@ -10,4 +10,17 @@ public class Pacman extends Movable{
 	public boolean stillAlive(){
 		return livesLeft > 0;
 	}
+	
+	private void keyPressed(KeyEvent e)
+	{
+		int key = e.getKeyCode();
+    		if (key == KeyEvent.VK_LEFT) {
+        		super.moveLeft();}
+		if (key == KeyEvent.VK_RIGHT) {
+			 Direction = 1;}
+		if (key == KeyEvent.VK_UP) {
+        		Direction = 2;}
+		if (key == KeyEvent.VK_DOWN) {
+        		Direction = 3;}
+	}
 }
